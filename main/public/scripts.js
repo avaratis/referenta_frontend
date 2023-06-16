c10ca74e6b9cd82494806f597f4d5639d8c28cf5
m.schwarzappel <you@example.com>
2023-06-17 00:56:01 +0200

    fixing loading screen

--- main/public/scripts.js
+++ main/public/scripts.js
@@ -177,17 +177,19 @@ console.log('File uploaded:', file);
 }
 
 function showLoadingScreen() {
-    const chatbox = document.getElementById('chatTabs');
     const loadingScreen = document.createElement('div');
-    loadingScreen.innerHTML = '<div class="loading-screen"><div class="loading-spinner"></div></div>';
+    loadingScreen.classList.add('loading-screen');
+    loadingScreen.innerHTML = `
+      <div class="loading-spinner">
+        <div class="spinner"></div>
+        <img class="logo" src="https://i.imgur.com/JIC16iz.png" alt="Logo">
+      </div>
+    `;
   
-    // Display the loading screen
-    chatbox.appendChild(loadingScreen);
-  
-    // Scroll to the bottom of the chatbox
-    chatbox.scrollTop = chatbox.scrollHeight;
+    const body = document.querySelector('body');
+    body.appendChild(loadingScreen);
   }
-
+  
 
 
 
