15343b65f1e858f8bc0993656fa9bceac4fc64f4
m.schwarzappel <you@example.com>
2023-06-17 14:51:27 +0200

    test

--- main/public/scripts.js
+++ main/public/scripts.js
@@ -106,7 +106,7 @@ const inputData = compileInputData();
                     const botReply = await response.text();
               
                     // Create a new tab for the chat reply
-                    const chatTabs = document.getElementById('navTabs');
+                    const chatTabs = document.getElementById('chatTabs');
                     const tabId = `tab-${Date.now()}`;
                     const tabButton = document.createElement('button');
                     tabButton.className = 'btn btn-light';
