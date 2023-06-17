267fd22be3ed944f4cd9c1ba780aaf781ab8b599
m.schwarzappel <you@example.com>
2023-06-17 03:02:28 +0200

    asdasd

--- main/public/scripts.js
+++ main/public/scripts.js
@@ -167,14 +167,34 @@ const file = e.dataTransfer.files[0];
 
 // Perform validation if needed (e.g., file type, size)
 
-// Handle the file upload
-handleFileUpload(file);
-});
-
-function handleFileUpload(file) {
-// You can perform additional processing or send the file to the server for further handling
-console.log('File uploaded:', file);
-}
+function handleFileUpload(event) {
+    const file = event.target.files[0]; // Get the selected file
+    const storageRef = firebase.storage().ref(); // Get the storage reference
+    
+    // Create a unique file name (e.g., using the user's ID or a random ID)
+    const fileName = generateUniqueFileName(file.name);
+  
+    // Upload the file to Firebase Storage
+    const uploadTask = storageRef.child(fileName).put(file);
+  
+    // Listen for upload progress or completion
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Handle progress updates (e.g., display a progress bar)
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        updateProgressBar(progress);
+      },
+      (error) => {
+        // Handle the upload error
+        console.error('Upload error:', error);
+      },
+      () => {
+        // Handle the upload completion
+        uploadComplete(uploadTask.snapshot.ref);
+      }
+    );
+  }
+  
 
 function showLoadingScreen() {
     const chatbox = document.getElementById('chatTabs');
