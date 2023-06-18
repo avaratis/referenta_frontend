9dfd048b9b82c84f41240c74954b64b1893a18af
m.schwarzappel <you@example.com>
2023-06-18 13:34:01 +0200

    4k tokens

--- main/functions/index.js
+++ main/functions/index.js
@@ -50,7 +50,7 @@ app.post('/getChatResponse', async (req, res) => {
             content: prompt,
           }
         ],
-        max_tokens: 10000,
+        max_tokens: 4000,
         
         
         /*functions: [
