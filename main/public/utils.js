23c051879107b4b84dc7ca66f22705f13780df78
m.schwarzappel <you@example.com>
2023-06-19 17:13:18 +0200

    added functioning dropzone

--- main/public/utils.js
+++ main/public/utils.js
@@ -1,55 +1,65 @@
-
-
 const dropzone = document.getElementById('dropzone');
+const pdfjsLib = window['pdfjs-dist/build/pdf'];
 
 // Prevent default behavior for drag and drop events
 dropzone.addEventListener('dragenter', (e) => {
-e.preventDefault();
-dropzone.classList.add('dragover');
+  e.preventDefault();
+  dropzone.classList.add('dragover');
 });
 
 dropzone.addEventListener('dragover', (e) => {
-e.preventDefault();
+  e.preventDefault();
 });
 
 dropzone.addEventListener('dragleave', (e) => {
-e.preventDefault();
-dropzone.classList.remove('dragover');
+  e.preventDefault();
+  dropzone.classList.remove('dragover');
 });
 
 dropzone.addEventListener('drop', (e) => {
-e.preventDefault();
-dropzone.classList.remove('dragover');
+  e.preventDefault();
+  dropzone.classList.remove('dragover');
+
+  // Retrieve the dropped file
+  const file = e.dataTransfer.files[0];
+
+  // Perform validation if needed (e.g., file type, size)
+  if (file.type !== 'application/pdf') {
+    console.error('File is not a PDF');
+    return;
+  }
+
+  // Create a new FileReader object
+  let reader = new FileReader();
+
+  // Define the event handler for the load event
+  reader.onload = function(event) {
+    let typedarray = new Uint8Array(this.result);
+
+    // Parse the PDF
+    pdfjsLib.getDocument(typedarray).promise.then(function(pdf) {
+      // Get the first page
+      pdf.getPage(1).then(function(page) {
+        // Extract the text content
+        page.getTextContent().then(function(textContent) {
+          // Log the text content to the console
+          console.log(textContent.items.map(item => item.str).join(' '));
+        }).catch(function(error) {
+          console.error('Error extracting text content:', error);
+        });
+      }).catch(function(error) {
+        console.error('Error getting page:', error);
+      });
+    }).catch(function(error) {
+      console.error('Error parsing PDF:', error);
+    });
+  };
+
+  // Define the event handler for the error event
+  reader.onerror = function(event) {
+    console.error('Error reading file:', event.target.error);
+  };
+
+  // Read the file as an ArrayBuffer
+  reader.readAsArrayBuffer(file);
 });
-// Retrieve the dropped file
-const file = e.dataTransfer.files[0];
-
-// Perform validation if needed (e.g., file type, size)
-
-function handleFileUpload(event) {
-    const file = event.target.files[0]; // Get the selected file
-    const storageRef = firebase.storage().ref(); // Get the storage reference
-    
-    // Create a unique file name (e.g., using the user's ID or a random ID)
-    const fileName = generateUniqueFileName(file.name);
-  
-    // Upload the file to Firebase Storage
-    const uploadTask = storageRef.child(fileName).put(file);
-  
-    // Listen for upload progress or completion
-    uploadTask.on('state_changed',
-      (snapshot) => {
-        // Handle progress updates (e.g., display a progress bar)
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        updateProgressBar(progress);
-      },
-      (error) => {
-        // Handle the upload error
-        console.error('Upload error:', error);
-      },
-      () => {
-        // Handle the upload completion
-        uploadComplete(uploadTask.snapshot.ref);
-      }
-    );
-    }
