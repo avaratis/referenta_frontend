73908af3afe846d0cfb2850b6f281862210bcbd5
m.schwarzappel <you@example.com>
2023-06-23 19:36:48 +0200

    updated function chaining

--- main/functions/index.js
+++ main/functions/index.js
@@ -42,7 +42,7 @@ app.post('/getChatResponse', async (req, res) => {
   let totalTokensGenerated = 0;
 
   try {
-    //while (totalTokensGenerated < totalTokensNeeded) {
+    while (totalTokensGenerated < totalTokensNeeded) {
       const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo-0613',
         messages: [
@@ -83,7 +83,7 @@ app.post('/getChatResponse', async (req, res) => {
       } else {
         throw new Error('Invalid response received from OpenAI API');
       }
-    //}
+    }
 
     res.send(fullResponse);
   } catch (error) {
