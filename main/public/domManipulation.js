065250a09198cfb7e827b3594a658b5d6a49e7aa
m.schwarzappel <you@example.com>
2023-07-06 01:48:02 +0200

    changed loading screen to modal

--- main/public/domManipulation.js
+++ main/public/domManipulation.js
@@ -1,25 +1,35 @@
+// Array of hints
+const hints = ["+486% durchschnittliche Produktivitätssteigerung im Test", "100.000+ verschiedene Textarten", "1-1000+ Geeignet für Einzelpersonen oder große Teams", "An den Nutzer angepasste und trainierte Large Language Models mit Hugging Transformers", "Trainiert auf 100.000 deutschen Reden"];
+
 function showLoadingScreen() {
-  ['chatTabs', 'chatTabs2', 'chatTabs3'].forEach((chatboxId) => {
-    const chatbox = document.getElementById(chatboxId);
-    const loadingScreen = document.createElement('div');
-    loadingScreen.innerHTML = '<div class="loading-screen"><div class="loading-spinner"></div></div>';
-    loadingScreen.className = 'loading-overlay';  // assign the new class
-  
-    // Display the loading screen
-    chatbox.appendChild(loadingScreen);
-  });
+  const modal = document.createElement('div');
+  modal.className = 'loading-modal';  // assign a new class
+  modal.innerHTML = `
+    <div class="loading-screen">
+      <div class="loading-spinner"></div>
+      <p id="hint-text"></p>
+    </div>
+  `;
+
+  // Display the modal
+  document.body.appendChild(modal);
+
+  // Start cycling through hints
+  let hintIndex = 0;
+  document.getElementById('hint-text').innerText = hints[hintIndex];
+  setInterval(() => {
+    hintIndex = (hintIndex + 1) % hints.length; // Loop back to the start when reaching the end
+    document.getElementById('hint-text').innerText = hints[hintIndex];
+  }, 5000); // Change hint every 5 seconds
 }
 
-
-  function hideLoadingScreen() {
-    ['chatTabs', 'chatTabs2', 'chatTabs3'].forEach((chatboxId) => {
-      const chatbox = document.getElementById(chatboxId);
-      const loadingOverlay = chatbox.querySelector('.loading-overlay');
-      if (loadingOverlay) {
-        chatbox.removeChild(loadingOverlay);
-      }
-    });
+function hideLoadingScreen() {
+  const modal = document.querySelector('.loading-modal');
+  if (modal) {
+    document.body.removeChild(modal);
   }
+}
+
 
   function showChatReply(tabId) {
     // Hide all chat replies
