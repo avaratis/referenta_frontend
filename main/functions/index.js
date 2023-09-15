d78a9a51c4a4c654c5c65fe7032f6f0931435d62
m.schwarzappel <you@example.com>
2023-09-15 12:45:42 +0200

    snyc2

--- main/functions/index.js
+++ main/functions/index.js
@@ -13,6 +13,7 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
+
 const functions = require('firebase-functions');
 const express = require('express');
 const bodyParser = require('body-parser');
